71752aa41342e8ef56e0cdbc13d1fb69c8469431
BrandonAWong <brandonwong2004@outlook.com>
2024-04-17 06:29:12 -0700

    balls

--- bucketlist/bucketList.cpp
+++ bucketlist/bucketList.cpp
@@ -5,20 +5,58 @@
 
 class Bucket {
     public:
-        Bucket();
-        void generate(int size, int min, int max);
-        void sort();
-        int size();
-        int atIndex(int);
-        int merge(Bucket b);
+        Bucket() { }
+
+        void generate(int size, int min, int max) {
+            for (std::size_t i {0}; i < size; ++i)
+               v.push_back(std::rand() % abs(min) - max - 1 + min);
+        }
+
+        void sort() {
+            int temp {};
+            for (std::size_t i {0}; i < size(); ++i) {
+            
+                for (std::size_t j {i+1}; j < size(); ++j)
+                    if (v[i] > v[j]) {
+                        temp = v[i];
+                        v[i] = v[j];
+                        v[j] = temp;
+                    }
+            }
+        }
+
+        int size() { return v.size(); }
+
+        int atIndex(int i) { return v[i]; }
+
+        void merge(Bucket b) {
+            int temp[size()+b.size()];
+                std::size_t i {0}, currentIndex1 {0}, currentIndex2 {0};
+                while (currentIndex1 < size() && currentIndex2 < b.size())
+                {
+                    temp[i++] =  (v[currentIndex1] < b.v[currentIndex2]) ? v[currentIndex1++] : b.v[currentIndex2++];
+                }
+                while (currentIndex1 < size())
+                    temp[i++] =  v[currentIndex1++];
+                while (currentIndex2 < b.size())
+                    temp[i++] =  b.v[currentIndex2++];
+
+                for (std::size_t i {0}; i < size()+b.size(); ++i) {
+                    if (i < size())
+                       v[i] = temp[i]; 
+                    else
+                        v.push_back(temp[i]);
+                }
+        }
 
     private:
         std::vector<int> v;
 };
 
+int globalSwapCount {0};
 int main(int argc, char *argv[])
 {
-    srand(time(0));
+    std::srand(std::time(0));
     int bucketCount = std::stoi(argv[1]);
     int bucketSize = std::stoi(argv[2]);
     int bucketMin = std::stoi(argv[3]);
@@ -29,7 +67,7 @@ int main(int argc, char *argv[])
     std::cout << "Bucket Max value:"<<bucketMax << '\n';
     std::vector<Bucket> list; // create empty Bucket vector
     Bucket *bptr;
-    Internalfor(int i {0}; i<bucketCount; i++) // creating bucketCount Buckets
+    for (int i {0}; i<bucketCount; i++) // creating bucketCount Buckets
     {
         bptr = new Bucket; // allocating new Bucket
         bptr->generate(bucketSize, bucketMin, bucketMax);
@@ -43,14 +81,16 @@ int main(int argc, char *argv[])
     Bucket endGame; // create empty Bucket to merge ALL buckets
     while (list.size() > 0) // std::vector<Bucket>::size()
     {
+        std::cout << "yo\n";
         endGame.merge(list[0]); // merge first bucket in list into endGame
         list.erase(list.begin()); // erase the first bucket in the list
     }
     // write all the numbers in endGame bucket to a file
-    fstream out("bucketList.out", ios::out);
+    std::fstream out("bucketList.out", std::ios::out);
     for(int i=0; i<endGame.size(); i++) // Bucket::size()
-        out << endGame.atIndex(i) << endl; // Bucket::atIndex(int)
+        out << endGame.atIndex(i) << std::endl; // Bucket::atIndex(int)
     std::cout << "Global Swap Count:"<< globalSwapCount << '\n';
     std::cout << "\nbucketList.out has "<<bucketCount * bucketSize<< " sorted numbers\n";
     return 0;
 }
+
